623d08335e39cc9d85ca28c91e99b7c70a878f6e
kb05 <luiscabov@gmail.com>
2024-03-24 13:34:06 +0100

    🧑‍💻 Use container name instead image name

--- docker-container-exec.sh
+++ docker-container-exec.sh
@@ -2,45 +2,42 @@
 
 #Global vars
 
-scripPath=$(dirname $(readlink -f "$0"));
-windowTitle="Docker-Container-Exec";
+scripPath=$(dirname $(readlink -f "$0"))
+windowTitle="Docker-Container-Exec"
 
 commandListPath="${scripPath}/commandList.json"
 
 #Get the container the container list and processed to be passed to the fzf search engine
-containerText=$(docker ps |  tr -s " " | cut -f 1-2 -d " " | tail -n +2 | fzf );
-container=$(echo $containerText |  cut -d " " -f1);
+containerText=$(docker ps --format '{{.ID}} {{.Names}}' | fzf)
+
+container=$(echo $containerText | cut -d " " -f1)
 
 # If no container has been select the scripts ends
-if [ -z "$container" ]
-then
-      exit;
+if [ -z "$container" ]; then
+      exit
 fi
 
 # Select the command to execute
-selectedCommandText=$(cat $commandListPath | jq -rs '.' | head -n-2 | tail -n+3| rev| sed 's/\(.*\),/\1 /' | cut -c1- | rev |  sed -e 's/^[ \t]*//' | fzf);
+selectedCommandText=$(cat $commandListPath | jq -rs '.' | head -n-2 | tail -n+3 | rev | sed 's/\(.*\),/\1 /' | cut -c1- | rev | sed -e 's/^[ \t]*//' | fzf)
 
 #Extract the command Key
 commandKey=$(echo {$selectedCommandText} | jq 'keys' | head -n-1 | tail -n+2 | xargs)
 
 # If no command has been select the scripts ends
-if [ -z "$commandKey" ]
-then
-      exit;
+if [ -z "$commandKey" ]; then
+      exit
 fi
 
-
 # Get the command template
-command=$(cat $commandListPath | jq ".$commandKey");
+command=$(cat $commandListPath | jq ".$commandKey")
 
 # # Replace the command vars
-command=${command//\$containerId/$container};
+command=${command//\$containerId/$container}
 
 # # Remove first and last "
-command="${command:1:${#command}-2}";
+command="${command:1:${#command}-2}"
 
 #Exec the command
 
 echo -e "\n\e[34m${command}\n\e[0m"
-$command;
-
+$command
